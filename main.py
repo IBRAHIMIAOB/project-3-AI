d7019a3f000a5843f50b837240f9207677e8489b
IBRAHIM ALOBAID <ibrahim.iaob@gmail.com>
2024-05-13 14:47:29 +0100

    update

--- main.py
+++ main.py
@@ -28,20 +28,16 @@ while True:
         r1 = np.arange(len(numbers1))
         r2 = [x + bar_width for x in r1]
 
-        # Create bar plot
         plt.bar(r1, numbers1, color='b', width=bar_width, edgecolor='grey', label='Silhouette Score')
         plt.bar(r2, numbers2, color='r', width=bar_width, edgecolor='grey', label='Davies-Bouldin score')
 
-        # Add xticks on the middle of the group bars
         plt.xlabel('Algorithms', fontweight='bold')
         plt.xticks([r + bar_width/2 for r in range(len(numbers1))], algorithms)
 
-        # Add labels and title
         plt.ylabel('Numbers')
         plt.title('Comparison of Algorithms')
         plt.legend()
 
-        # Show plot
         plt.show()
         
         print("givin the Plot DBSCAN is The best Clustering algorithm for Spine DataSet since it has High Silhouette Score and low Davies-Bouldin Score \n ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ ")
